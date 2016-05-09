9f9bae49b39f9c487fc0327fe59f8df19d0a594e
amashrabov <amashrabov@gmail.com>
2016-05-09 23:27:03 +0300

    Gauss (from e-maxx)

--- numtheory.cpp
+++ numtheory.cpp
@@ -86,6 +86,50 @@ int generator (int p) {
 	return -1;
 }
 
+int gauss (vector < vector<double> > a, vector<double> & ans) {
+	const int inf = 1e9; const double eps = 1e-8;
+	int n = (int) a.size();
+	int m = (int) a[0].size() - 1;
+ 
+	vector<int> where (m, -1);
+	for (int col=0, row=0; col<m && row<n; ++col) {
+		int sel = row;
+		for (int i=row; i<n; ++i)
+			if (abs (a[i][col]) > abs (a[sel][col]))
+				sel = i;
+		if (abs (a[sel][col]) < eps)
+			continue;
+		for (int i=col; i<=m; ++i)
+			swap (a[sel][i], a[row][i]);
+		where[col] = row;
+ 
+		for (int i=0; i<n; ++i)
+			if (i != row) {
+				double c = a[i][col] / a[row][col];
+				for (int j=col; j<=m; ++j)
+					a[i][j] -= a[row][j] * c;
+			}
+		++row;
+	}
+ 
+	ans.assign (m, 0);
+	for (int i=0; i<m; ++i)
+		if (where[i] != -1)
+			ans[i] = a[where[i]][m] / a[where[i]][i];
+	for (int i=0; i<n; ++i) {
+		double sum = 0;
+		for (int j=0; j<m; ++j)
+			sum += ans[j] * a[i][j];
+		if (abs (sum - a[i][m]) > eps)
+			return 0;
+	}
+ 
+	for (int i=0; i<m; ++i)
+		if (where[i] == -1)
+			return inf;
+	return 1;
+}
+
 int main() {
 	return 0;
 }
