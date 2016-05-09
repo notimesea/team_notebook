45ffafdd831d5a3568f72edefa51e2ff79024f41
amashrabov <amashrabov@gmail.com>
2016-05-09 21:44:20 +0300

    Add tests

--- bridges.cpp
+++ bridges.cpp
@@ -1,31 +1,36 @@
 #include <bits/stdc++.h>
+#define forn(i, n) for(int i = 0; i < (int)(n); ++i)
+#define pb push_back
 using namespace std;
 typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpi;
 
 const int maxn = 100500;
-int n, m, tin[maxn], timer = 0, fup[maxn], ans[maxn], numComps = 0;
+int tin[maxn], timer = 0, fup[maxn], ans[maxn], numComps = 0;
 struct Edge{
-	int id, tar;
+	int id, to;
 };
 vi st;
 vector<Edge> g[maxn];
 bool used[maxn];
-void dfs(int v, int p) {
+bool isBridge[maxn];
+
+void dfs(int v, int p = -1) {
 	used[v] = true;
-	timer++;
-	fup[v] = tin[v] = timer;
+	fup[v] = tin[v] = timer++;
 	st.push_back(v);
-	for (int i = 0; i < (int)g[v].size(); i++) {
+	forn (i, g[v].size()) {
 		if (g[v][i].id == p)
 			continue;
-		int to = g[v][i].tar;
+		int to = g[v][i].to;
 		if (used[to]) 
 			fup[v] = min(fup[v], tin[to]);
 		else {
 			dfs(to, g[v][i].id);
 			if (fup[to] > tin[v]) { 
 				numComps++;
-				//IS_BRIDGE(to, v);
+				isBridge[g[v][i].id] = true;
 				while (st.size()) {
 					int x = st.back(); st.pop_back();
 					ans[x] = numComps;
@@ -38,6 +43,50 @@ void dfs(int v, int p) {
 	} 
 }
 
+void testQuality() {
+	int n = 3 + rand() % 100;
+	forn (i, n) g[i].clear(), used[i] = 0; numComps = 0;
+	int m = 1 + rand() % (n * n / 3);
+	forn (i, m) isBridge[i] = 0;
+	vpi edges(m);
+	forn (i, m) {
+		int x = rand() % n, y = rand() % n;
+		edges[i] = make_pair(x, y);
+		g[x].pb(Edge{i, y}); g[y].pb(Edge{i, x});
+	}
+	int comps = 0;
+	forn (i, n) 
+		if (!used[i]) {
+			dfs(i);
+			++comps;
+			if (st.size()) {
+				numComps++;
+				for (int x: st) ans[x] = numComps;
+				st.clear();
+			}
+		}
+	forn (b, m) {
+		vi used(n);
+		int cur = 0;
+		forn (i, n)
+			if (!used[i]) {
+				cur++;
+				queue<int> q;
+				q.push(i); used[i] = 1;
+				while (!q.empty()) {
+					int v = q.front(); q.pop();
+					for (auto e: g[v])
+						if (!used[e.to] && e.id != b) {
+							q.push(e.to);
+							used[e.to] = 1;
+						}
+				}
+			}
+		assert((comps < cur) == isBridge[b]);
+	}
+}
+
 int main() {
+	forn (t, 1000) testQuality();
 	return 0;
 }
--- cut_point.cpp
+++ cut_point.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-
+#define forn(i, n) for(int i = 0; i < (int)(n); ++i)
 #define pb push_back
 using namespace std;
 typedef vector<int> vi;
@@ -7,9 +7,9 @@ typedef pair<int, int> pii;
 typedef vector<pii> vpi;
 
 const int maxn = 100500;
-int n, m, tin[maxn], timer = 0, fup[maxn], ans[maxn], numComps = 0;
+int tin[maxn], timer = 0, fup[maxn];
 struct Edge{
-	int id, tar;
+	int id, to;
 };
 vector<Edge> g[maxn];
 bool used[maxn];
@@ -18,21 +18,17 @@ vpi st;
 vector<vpi> components;
 void dfs(int v, int p = -1){
     used[v] = true;
-    timer++;
-    fup[v] = tin[v] = timer;
-    int kol = 0;
-	for (int i = 0; i < (int)g[v].size(); i++) {
-	    if (g[v][i].id == p)
+    fup[v] = tin[v] = timer++;
+    int k = 0;
+	forn(i, g[v].size()) {
+	    if (g[v][i].to == p)
 			continue;
-		int to = g[v][i].tar;
-		
-        if (to == p)
-            continue;
+		int to = g[v][i].to;
         if (used[to])
             fup[v] = min(fup[v], tin[to]);
         else
         {
-            ++kol;
+            ++k;
             pii cedge(v, to);
             st.pb(cedge);
             dfs(to, v);
@@ -55,10 +51,52 @@ void dfs(int v, int p = -1){
         }
     }
     if (p == -1)
-        isCutPoint[v] = (kol > 1);
+        isCutPoint[v] = (k > 1);
+}
+
+void testQuality() {
+	int n = 3 + rand() % 20; components.clear();
+	forn (i, n) g[i].clear(), used[i] = 0, isCutPoint[i] = 0; 
+	int m = 1 + rand() % (n * n / 3);
+	vpi edges(m);
+	forn (i, m) {
+		int x = rand() % n, y = rand() % n;
+		edges[i] = make_pair(x, y);
+		g[x].pb(Edge{i, y}); g[y].pb(Edge{i, x});
+	}
+	int comps = 0;
+	forn (i, n) 
+		if (!used[i]) {
+			dfs(i);
+			++comps;
+			if (st.size()) {
+				components.pb(st);
+				st.clear();
+			}
+		}
+	forn (b, n) {
+		vi used(n);
+		used[b] = 1;
+		int cur = 0;
+		forn (i, n)
+			if (!used[i]) {
+				cur++;
+				queue<int> q;
+				q.push(i); used[i] = 1;
+				while (!q.empty()) {
+					int v = q.front(); q.pop();
+					for (auto e: g[v])
+						if (!used[e.to]) {
+							q.push(e.to);
+							used[e.to] = 1;
+						}
+				}
+			}
+		assert((comps < cur) == isCutPoint[b]);
+	}
 }
 
 int main() {
-	
+	forn (t, 100) testQuality();
 	return 0;
 }
